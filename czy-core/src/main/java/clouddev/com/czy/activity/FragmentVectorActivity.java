4019c1e9ac38a8e3a5decc396b3102fb51710c2d
ZeyvCui <qzhjpsl@mail.dlut.edu.cn>
2017-12-23 16:13:35 +0800

    After C4

---
+++ czy-core/src/main/java/clouddev/com/czy/activity/FragmentVectorActivity.java
@@ -0,0 +1,46 @@
+package clouddev.com.czy.activity;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.ContentFrameLayout;
+import android.util.Log;
+
+import clouddev.com.czy.R;
+import clouddev.com.czy.fragment.CoreFragment;
+import me.yokeyword.fragmentation.SupportActivity;
+
+/**
+ * Created by 29737 on 2017/12/23.
+ */
+
+public abstract class FragmentVectorActivity extends SupportActivity
+{
+   public abstract CoreFragment setRootFragment();
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        init(savedInstanceState);
+    }
+
+    private void init(@Nullable Bundle savedInstanceState)
+    {
+        final ContentFrameLayout container=new ContentFrameLayout(this);
+        container.setId(R.id.delegate_container);
+        setContentView(container);
+        if(savedInstanceState==null)
+        {
+            loadRootFragment(R.id.delegate_container,setRootFragment());
+        }
+    }
+
+    @Override
+    protected void onDestroy()
+    {
+        super.onDestroy();
+        System.gc();
+        System.runFinalization();
+    }
+}
---
+++ czy-core/src/main/java/clouddev/com/czy/fragment/PermissionCheckFragment.java
@@ -0,0 +1,10 @@
+package clouddev.com.czy.fragment;
+
+/**
+ * Created by 29737 on 2017/12/23.
+ */
+
+public abstract class PermissionCheckFragment extends BaseFragment
+{
+
+}
